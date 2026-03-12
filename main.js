9c2a07b427f57a56b17076bd270417a53004897c
Tiago Ribeiro Pereira <tiagorptb24@gmail.com>
2026-03-12 19:29:50 -0300

    feat: Implemented carousel

--- main.js
+++ main.js
@@ -83,72 +83,72 @@ function renderProgression(env) {
 
 	const finalInvestmentObject = returnsArray[returnsArray.length - 1];
 
-	// doughnutChartReference = new Chart(finalMoneyChart, {
-	// 	type: "doughnut",
-	// 	data: {
-	// 		labels: ["Total Investido", "Rendimento", "Imposto"],
-	// 		datasets: [
-	// 			{
-	// 				data: [
-	// 					formatCurrencyToGraph(
-	// 						finalInvestmentObject.investedAmount,
-	// 					),
-	// 					formatCurrencyToGraph(
-	// 						finalInvestmentObject.totalInterestReturns *
-	// 							(1 - taxRate / 100),
-	// 					),
-	// 					formatCurrencyToGraph(
-	// 						finalInvestmentObject.totalInterestReturns *
-	// 							(taxRate / 100),
-	// 					),
-	// 				],
-	// 				backgroundColor: [
-	// 					"rgb(255, 99, 132)",
-	// 					"rgb(54, 162, 235)",
-	// 					"rgb(255, 205, 86)",
-	// 				],
-	// 				hoverOffset: 4,
-	// 			},
-	// 		],
-	// 	},
-	// });
+	doughnutChartReference = new Chart(finalMoneyChart, {
+		type: "doughnut",
+		data: {
+			labels: ["Total Investido", "Rendimento", "Imposto"],
+			datasets: [
+				{
+					data: [
+						formatCurrencyToGraph(
+							finalInvestmentObject.investedAmount,
+						),
+						formatCurrencyToGraph(
+							finalInvestmentObject.totalInterestReturns *
+								(1 - taxRate / 100),
+						),
+						formatCurrencyToGraph(
+							finalInvestmentObject.totalInterestReturns *
+								(taxRate / 100),
+						),
+					],
+					backgroundColor: [
+						"rgb(255, 99, 132)",
+						"rgb(54, 162, 235)",
+						"rgb(255, 205, 86)",
+					],
+					hoverOffset: 4,
+				},
+			],
+		},
+	});
 
-	// progressionChartReference = new Chart(progressionChart, {
-	// 	type: "bar",
-	// 	data: {
-	// 		labels: returnsArray.map(
-	// 			(investmentObject) => investmentObject.month,
-	// 		),
-	// 		datasets: [
-	// 			{
-	// 				label: "Total Investido",
-	// 				data: returnsArray.map((investmentObject) =>
-	// 					formatCurrencyToGraph(investmentObject.investedAmount),
-	// 				),
-	// 				backgroundColor: "rgb(255, 99, 132)",
-	// 			},
-	// 			{
-	// 				label: "Retorno do Investimento",
-	// 				data: returnsArray.map((investmentObject) =>
-	// 					formatCurrencyToGraph(investmentObject.interestReturns),
-	// 				),
-	// 				backgroundColor: "rgb(54, 162, 235)",
-	// 			},
-	// 		],
-	// 	},
-	// 	options: {
-	// 		responsive: true,
-	// 		scales: {
-	// 			x: {
-	// 				stacked: true,
-	// 			},
+	progressionChartReference = new Chart(progressionChart, {
+		type: "bar",
+		data: {
+			labels: returnsArray.map(
+				(investmentObject) => investmentObject.month,
+			),
+			datasets: [
+				{
+					label: "Total Investido",
+					data: returnsArray.map((investmentObject) =>
+						formatCurrencyToGraph(investmentObject.investedAmount),
+					),
+					backgroundColor: "rgb(255, 99, 132)",
+				},
+				{
+					label: "Retorno do Investimento",
+					data: returnsArray.map((investmentObject) =>
+						formatCurrencyToGraph(investmentObject.interestReturns),
+					),
+					backgroundColor: "rgb(54, 162, 235)",
+				},
+			],
+		},
+		options: {
+			responsive: true,
+			scales: {
+				x: {
+					stacked: true,
+				},
 
-	// 			y: {
-	// 				stacked: true,
-	// 			},
-	// 		},
-	// 	},
-	// });
+				y: {
+					stacked: true,
+				},
+			},
+		},
+	});
 
 	createTable(columnArray, returnsArray, "results-table");
 }
@@ -218,6 +218,19 @@ for (const formElement of form) {
 		formElement.addEventListener("blur", validateInput);
 	}
 }
+
+const mainEl = document.querySelector("main");
+const carouselEl = document.getElementById("carousel");
+const nextButton = document.getElementById("slide-arrow-next");
+const previousButton = document.getElementById("slide-arrow-previous");
+
+nextButton.addEventListener("click", () => {
+	carouselEl.scrollLeft += mainEl.clientWidth;
+});
+
+previousButton.addEventListener("click", () => {
+	carouselEl.scrollLeft -= mainEl.clientWidth;
+});
 
 form.addEventListener("submit", renderProgression);
 clearFormButton.addEventListener("click", clearForm);
